83bc3cc50b32a8ea5bde3e99949b78584de662a9
venkata.mannem <venkataprasad431@gmail.com>
2021-07-25 17:01:45 -0400

    fixed HTTP OK Issue

--- lib/ui/core/utilities/constants/strings.dart
+++ lib/ui/core/utilities/constants/strings.dart
@@ -3,7 +3,7 @@ class Strings {
   static const String kwebPage = '/newswebview';
 
   static const int HTTP_SUCCESS = 200;
-
+  static const String HTTP_OK = 'ok';
   static const String KSELECTED_THEME = 'kthemename';
 
   static String apikey = '94d4e29b2b1b4b25b30a9b8d8fbb4c85';
